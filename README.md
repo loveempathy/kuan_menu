27ee7d502c4943e86c1a238363c0506f139d6e7a
LoveEmpathy <1403697854@qq.com>
2019-08-26 18:52:24 +0800

    Signed-off-by: LoveEmpathy <1403697854@qq.com>

---
+++ README.md
@@ -0,0 +1,16 @@
+# 仿酷安app发布按钮动效
+
+> 以前写的一个代码，稍微完善了下，代码比较简单，用到了css动画跟小程序Animation，完善了下注释。多的不说了，代码比较简陋，直接看效果图吧
+
+## 效果图
+![效果图][1]
+
+  [1]: https://loveempathy.com/usr/uploads/2019/08/1755250036.gif
+## 序言
+> 部分css代码参考网上代码
+
+## 源码
+> 腾讯代码托管 [去看看](https://dev.tencent.com/u/LoveEmpathy/p/kuan_menu/git)
+
+## 博客
+> LoveEmpathy [去看看](https://loveempathy.com)
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,90 +1,103 @@
 const app = getApp();
-var isShow = false;
-var animation;
-var height = wx.getSystemInfoSync().windowHeight;
+//是否显示 默认不显示
+var isShow = false,
+	//动画对象实例
+	animation,
+	//获取当前设备的高度
+	height = wx.getSystemInfoSync().windowHeight;
 Page({
-  data: {
-    StatusBar: app.globalData.StatusBar,
-    CustomBar: app.globalData.CustomBar,
-    height: height,
-    hidden: true,
-    isShow: isShow,
-    myStyle: "border-radius: " + height + "px;height: " + height / 15 + "px;width: " + height / 15 + "px;bottom:" + height / 15 + "px",
-    nav: [{
-      navigation: [{
-          name: '动态',
-          src: '../../img/1.png'
-        },
-        {
-          name: '酷图',
-          src: '../../img/2.png'
-        },
-        {
-          name: '应用集',
-          src: '../../img/3.png'
-        },
-        {
-          name: '扫一扫',
-          src: '../../img/4.png'
-        },
+	data: {
+		StatusBar: app.globalData.StatusBar, //状态栏高度
+		CustomBar: app.globalData.CustomBar, //titleBar高度
+		hidden: true, //默认为隐藏
+		isShow,
+		//默认为圆形    宽高为设备高度÷15      
+		myStyle: "border-radius: " +
+			height + "px;height: " +
+			height / 15 + "px;width: " +
+			height / 15 + "px;",
+		nav: [{
+			navigation: [{
+					name: '动态',
+					src: '../../img/1.png'
+				},
+				{
+					name: '酷图',
+					src: '../../img/2.png'
+				},
+				{
+					name: '应用集',
+					src: '../../img/3.png'
+				},
+				{
+					name: '扫一扫',
+					src: '../../img/4.png'
+				},
 
-      ],
-    }, {
-      navigation: [{
-          name: '分享',
-          src: '../../img/5.png'
-        },
-        {
-          name: '图文',
-          src: '../../img/6.png'
-        },
-        {
-          name: '提问',
-          src: '../../img/7.png'
-        },
-        {
-          name: '话题',
-          src: '../../img/8.png'
-        },
+			],
+		}, {
+			navigation: [{
+					name: '分享',
+					src: '../../img/5.png'
+				},
+				{
+					name: '图文',
+					src: '../../img/6.png'
+				},
+				{
+					name: '提问',
+					src: '../../img/7.png'
+				},
+				{
+					name: '话题',
+					src: '../../img/8.png'
+				},
 
-      ],
-    }]
-  },
-  onLoad: function(options) {
-    animation = wx.createAnimation({
-      duration: 300,
-      timingFunction: 'linear',
-    })
-  },
+			],
+		}]
+	},
+	onLoad: function(options) {
+		//初始化动画
+		animation = wx.createAnimation({
+			duration: 300,
+			timingFunction: 'linear',
+		})
+	},
 
-  onClickPaper: function(e) {
-    var menuStyle = ''
-    var that = this
-    that.animation = animation
-    that.setData({
-      hidden: false,
-      menuStyle: menuStyle,
-    })
-    if (!isShow) {
-      that.animation.scale(height / 15).step()
-      menuStyle = 'menuOpen'
-    } else {
-      that.animation.scale(0).step()
-      menuStyle = 'menuClose'
-    }
-    isShow = !isShow
-    that.setData({
-      animationData: that.animation.export(),
-      menuStyle: menuStyle,
-    })
-    isShow?
-    setTimeout(function(){
-      that.setData({
-        isShow: isShow,
-      })
-      }, 200) : that.setData({
-        isShow: isShow,
-      })
-  },
+	onClickAdd: function(e) {
+		var menuStyle = ''
+		var that = this
+		that.animation = animation
+		that.setData({
+			hidden: false, //隐藏白色面板(ripple)
+			menuStyle: menuStyle, //设置底部加号按钮style
+		})
+		//判断是否显示
+		if (!isShow) {
+			//未显示 则执行动画 缩放设备高度÷15高度
+			that.animation.scale(height / 15).step()
+			//加号按钮执行打开动画
+			menuStyle = 'menuOpen'
+		} else {
+			//已显示 则执行动画 缩放回0
+			that.animation.scale(0).step()
+			//加号按钮执行关闭动画
+			menuStyle = 'menuClose'
+		}
+		isShow = !isShow //存储显示状态
+		that.setData({
+			animationData: that.animation.export(), //动画赋值
+			menuStyle: menuStyle, //加号按钮style赋值
+		})
+		//如果显示状态为true 延时200毫秒后执行内容显示 否则立即隐藏
+		isShow ?
+			setTimeout(function() {
+				that.setData({
+					isShow
+				})
+			}, 200) : that.setData({
+				isShow
+			})
+	},
 
 })
